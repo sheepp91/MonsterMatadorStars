34b7e5caae5626815c599a1b5ae02fa5e871903d
AIE\s171220 <joshua_laurence@live.com>
2017-06-29 10:22:28 +1000

    Monster Move

---
+++ Assets/Scripts/MonsterScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterScript : MonoBehaviour {
+
+    public float speed;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        Vector3 temp = this.transform.position;
+        temp.x += speed;
+        this.transform.position = temp;
+    }
+}
