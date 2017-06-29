e3a6392c825cbe99b8ceec7e4c79d034d7b612ae
AIE\s171220 <joshua_laurence@live.com>
2017-06-29 10:48:26 +1000

    Indicated when player is hit Added Monster Prefabs

--- Assets/Scripts/MonsterScript.cs
+++ Assets/Scripts/MonsterScript.cs
@@ -7,14 +7,20 @@ public class MonsterScript : MonoBehaviour {
     public Transform player;
     public float speed;
 
+    [HideInInspector]
+    public bool hitPlayer = false;
+
     private Vector3 playerPos;
     private Vector3 initialMonsterPos;
+    private Vector3 finalPos;
     private float startTime;
     private float journeyLength;
 
     void Start () {
         playerPos = player.position;
         initialMonsterPos = transform.position;
+        finalPos = playerPos;
+        finalPos.x += 5;
         startTime = Time.time;
         journeyLength = Vector3.Distance(initialMonsterPos, playerPos);
     }
@@ -22,6 +28,11 @@ public class MonsterScript : MonoBehaviour {
 	void Update () {
         float distCovered = (Time.time - startTime) * speed;
         float fracJourney = distCovered / journeyLength;
-        transform.position = Vector3.Lerp(initialMonsterPos, playerPos, fracJourney);
+        transform.position = Vector3.Lerp(initialMonsterPos, finalPos, fracJourney);
+
+        if (transform.position.x >= playerPos.x) {
+            hitPlayer = true;
+            print("Player hit!");
+        }
     }
 }
