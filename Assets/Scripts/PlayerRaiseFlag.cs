f793a1c7d4ba3a52f7b332c8fd113fa1e325ffce
AIE\s171220 <joshua_laurence@live.com>
2017-06-29 12:14:36 +1000

    Player lifts flag and doesn't get hit. Player doesn't lift flag. Gets hit.

---
+++ Assets/Scripts/PlayerRaiseFlag.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRaiseFlag : MonoBehaviour {
+    [HideInInspector]
+    public bool raisedFlag = false;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		if (Input.GetKeyDown("space")) {
+            print("Lift flag!");
+            raisedFlag = true;
+        }
+	}
+}
