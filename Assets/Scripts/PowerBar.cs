8d310443134608b2ccc1a63c67bad734c144bf04
AIE\s171235 <snowy844@gmail.com>
2017-06-29 11:38:41 +1000

    Ui and power bar

---
+++ Assets/Scripts/PowerBar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerBar : MonoBehaviour {
+
+    public Transform LoadingBar;
+    Color Bar;
+    [SerializeField] private float currentAmount;
+    [SerializeField]private float speed;
+    // Use this for initialization
+    void Start () {
+        Bar = LoadingBar.GetComponent<Image>().color;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (currentAmount < 100)
+        {
+            currentAmount += speed * Time.deltaTime;
+            if (currentAmount > 90 && currentAmount < 100)
+            {
+                Bar.g = 255;
+                Bar.r = 0;
+                Bar.b = 0;
+               
+            }
+        }
+        LoadingBar.GetComponent<Image>().fillAmount = currentAmount / 100;
+
+    }
+}
