1d12a3f51f297a53567d354f1931ced9af3161c2
AIE\s171235 <snowy844@gmail.com>
2017-06-30 12:11:39 +1000

    playersounds

---
+++ Assets/Scripts/PlaySounds.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySounds : MonoBehaviour {
+
+    AudioSource[] monstersteps;
+    AudioSource step1;
+    AudioSource step2;
+    AudioSource step3;
+    AudioSource step4;
+    AudioSource step5;
+    AudioSource step6;
+
+
+    void Start () {
+        monstersteps = GetComponents<AudioSource>();
+        step1 = monstersteps[0];
+        step2 = monstersteps[1];
+        step3 = monstersteps[2];
+        //step4 = monstersteps[3];
+        //step5 = monstersteps[4];
+        //step6 = monstersteps[5];
+    }
+	
+	
+   public void playSounds() {
+        step1.Play();
+        step2.Play();
+        step3.Play();
+        step4.Play();
+    }
+}
